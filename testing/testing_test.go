d8b5a45c0c8349f69855a810be2b3f71ebb6a0f1
Robin Joseph <robin@lob.com>
2018-03-07 18:45:12 -0800

    avoid possible failure with rand test

--- testing/testing_test.go
+++ testing/testing_test.go
@@ -3,9 +3,9 @@ package testing
 import "testing"
 
 func TestRandStr(t *testing.T) {
-	for i := 1; i <= 30; i++ {
-		str1 := RandStr(i)
-		str2 := RandStr(i)
+	for i := 0; i <= 40; i++ {
+		str1 := RandStr(16)
+		str2 := RandStr(16)
 
 		if str1 == str2 {
 			t.Errorf("%s should not equal %s", str1, str2)
