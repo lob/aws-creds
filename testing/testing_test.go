bc9156a66f4dfb7a4ce241c433067a0da0711478
Robin Joseph <robin@lob.com>
2018-03-08 14:43:30 -0800

    assert length in RandStr test

--- testing/testing_test.go
+++ testing/testing_test.go
@@ -3,10 +3,17 @@ package testing
 import "testing"
 
 func TestRandStr(t *testing.T) {
+	length := 16
 	for i := 0; i <= 40; i++ {
-		str1 := RandStr(16)
-		str2 := RandStr(16)
+		str1 := RandStr(length)
+		str2 := RandStr(length)
 
+		if len(str1) != length {
+			t.Errorf("%s should have %d characters", str1, length)
+		}
+		if len(str2) != length {
+			t.Errorf("%s should have %d characters", str2, length)
+		}
 		if str1 == str2 {
 			t.Errorf("%s should not equal %s", str1, str2)
 		}
