b9f3e4e26908a5a11c152788c149ce691241c365
Robin Joseph <robin@lob.com>
2018-03-07 18:13:15 -0800

    nvm changing it to conf instead

--- config/config.go
+++ config/config.go
@@ -8,8 +8,8 @@ import (
 	"path/filepath"
 )
 
-// Configuration contains the global configuration of this CLI
-type Configuration struct {
+// Config contains the global configuration of this CLI
+type Config struct {
 	Username            string     `json:"username"`
 	OktaOrgURL          string     `json:"okta_org_url"`
 	PreferredFactorType string     `json:"preferred_factor_type"`
@@ -22,16 +22,16 @@ type Profile struct {
 	RoleArn string `json:"role_arn"`
 }
 
-// Config is the global configuration shared across the app
-var Config = &Configuration{}
+// Conf is the global configuration shared across the app
+var Conf = &Config{}
 
 // ErrNotConfigured is thrown if this CLI hasn't been configured yet
 var ErrNotConfigured = errors.New("aws-creds hasn't been configured yet")
 
 var jsonMarshalIndent = json.MarshalIndent
 
-// Load data from the config file into the Configuration struct
-func (config *Configuration) Load(configFile string) error {
+// Load data from the config file into the Config struct
+func (conf *Config) Load(configFile string) error {
 	if _, err := os.Stat(configFile); os.IsNotExist(err) {
 		return ErrNotConfigured
 	}
@@ -43,18 +43,18 @@ func (config *Configuration) Load(configFile string) error {
 		return ErrNotConfigured
 	}
 
-	return json.Unmarshal(raw, &config)
+	return json.Unmarshal(raw, &conf)
 }
 
-// Save data from the Configuration struct into the config file
-func (config *Configuration) Save(configFile string) error {
+// Save data from the Config struct into the config file
+func (conf *Config) Save(configFile string) error {
 	path := filepath.Dir(configFile)
 	err := os.MkdirAll(path, 0700)
 	if err != nil {
 		return err
 	}
 
-	raw, err := jsonMarshalIndent(config, "", "  ")
+	raw, err := jsonMarshalIndent(conf, "", "  ")
 	if err != nil {
 		return err
 	}
